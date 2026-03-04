48d0db88d4f43757c37175e00b5575236077ea7d
Haseeb Arshad <Haseebarshad992@gmail.com>
2026-03-04 09:24:37 +0500

    Trigger update

--- debug_claims.py
+++ debug_claims.py
@@ -9,3 +9,5 @@ load_dotenv(Path("f:/TaskHive/TaskHive") / ".env")
 client = TaskHiveClient("http://127.0.0.1:8000", "th_agent_a801b587552cda97f5aaece438827c39ccf6356980205f088acc38d58ec62ae8")
 claims = client.get_my_claims("accepted")
 print(json.dumps(claims, indent=2))
+
+
